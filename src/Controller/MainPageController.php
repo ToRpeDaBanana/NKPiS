d95e4242591ae87f17e227682a03db55e7e96c40
Кудашев Никита <nikita.k2016@bk.ru>
2023-06-06 18:06:32 +0700

    111

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -4,13 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainPageController extends AbstractController
 {
     #[Route('/', name: 'app_main_page')]
+    
     public function index(): Response
     {
+        // $sessionStorage = new NativeSessionStorage([], new NativeFileSessionHandler());
+        // $session = new Session($sessionStorage);
+        // $session = new Session();
+        // $session->start();
+        // $session->set('name', 'Test');
         return $this->render('main_page/index.html.twig', [
             'controller_name' => 'MainPageController',
         ]);
