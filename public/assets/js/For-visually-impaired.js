869091df0468f7d425ea540c609067fda841f5da
Кудашев Никита <nikita.k2016@bk.ru>
2023-06-07 17:58:03 +0700

    Js

--- public/assets/js/For-visually-impaired.js
+++ public/assets/js/For-visually-impaired.js
@@ -9,9 +9,13 @@ function replaceClass(id, oldClass,oldClass2,oldClass3,newClass) {
     elem.addClass(newClass);
 }
 var customObject = {
-	fontSize: 10,
-	font: "Font Awesome 6 Free",
+	fontSize: 16,
+	font: 'PT Sans',
 	display: 'hide',
+	checkboxInterval: 'normal',
+	betweenInterval: 1,
+	colorSite: 'none',
+	imgVisible: 'block',
 };
 function ajaxSend(){
 	$.ajax({
@@ -27,6 +31,26 @@ function ajaxSend(){
 		}
 	});
 }
+function customStyle(){
+	if(myData1.display == 'active'){
+		$('.conteiner-for-visually-impaired').toggleClass('active');
+		customObject.display = "active";
+		ajaxSend();
+		//Font-Size
+			$('.gos').css("font-size",(myData1.fontSize ? myData1.fontSize : 16)+"px");
+			$('.').css("font-size",(myData1.fontSize ? myData1.fontSize : 16)+"px");
+		//*
+	}
+	else if (myData1.display == 'hide'){
+
+
+	}
+}
+
+
+
+
+
 $(document).ready(function() {
 	console.log(myData1);
 	console.log(myData1.fontSize);
@@ -38,20 +62,43 @@ $(document).ready(function() {
 	if (myData1.display == 'active'){
 		$('.conteiner-for-visually-impaired').toggleClass('active');
 		// $('html').addClass('hfont-size-small hfont-classic hcheckbox-interval-f hbetween-interval-small hcolor-site-white-black hcheckbox-img-t');
-		$('html').css("font-size",(myData1.fontSize ? myData1.fontSize : 15)+"px");
-
+		$('html').css("font-size",(myData1.fontSize ? myData1.fontSize : 16)+"px");
+		$('p').css("font-family",(myData1.font ? myData1.font : 'PT Sans'));
+		$('html').css('letter-spacing',(myData1.checkboxInterval ? myData1.checkboxInterval :'normal'));
+		$('p').css('line-height',myData1.betweenInterval ? myData1.betweenInterval : 1);
+		$('img').css('display',(myData1.imgVisible ? myData1.imgVisible : 'block'));
 		$('.for-visually-impaired').css("display","none");
-
+		customObject.display = "active";
+		ajaxSend();
 		$('.conteiner-for-visually-impaired.active').css("display","block");
 
 		$('.classic-site-version-btn').css("display","block");
+		$('.switch-btn').toggleClass('switch-on');
+		if(myData1.checkboxInterval == '.1rem'){
+			$('.switch-btn').trigger('on.switch');
+		}
+		else if(myData1.checkboxInterval == 'normal'){
+			$('.switch-btn.switch-on').toggleClass('switch-on');
+			$('.switch-btn').trigger('off.switch');
+		}
 
+		$('.switch-btn2').toggleClass('switch-on2');
+		if(myData1.imgVisible == 'block'){
+			$('.switch-btn2').trigger('on.switch');
+		}
+		else if(myData1.imgVisible == 'none'){
+			$('.switch-btn2.switch-on2').toggleClass('switch-on2');
+			$('.switch-btn2').trigger('off.switch');
+		}
 	}
 	else if (myData1.display == 'hide'){
 		$('.conteiner-for-visually-impaired.active').toggleClass('active');
-
-		$('html').css("font-size","15px");
-
+		customObject.display = "hide";
+		ajaxSend();
+		$('html').css("font-size","16px");
+		$('html').css('letter-spacing','normal');
+		$('p').css("font-family",'PT Sans');
+		$('p').css('line-height',myData1.betweenInterval ? myData1.betweenInterval : 1);
 		// $('html').addClass('hfont-size-small hfont-classic hcheckbox-interval-f hbetween-interval-small hcolor-site-white-black hcheckbox-img-t');
 		var impairedStyle = $(".for-visually-impaired").prop("style");
 		impairedStyle.removeProperty("display");
@@ -60,28 +107,39 @@ $(document).ready(function() {
 
 		$('.classic-site-version-btn').css("display","none");
 
+		$('.switch-btn.switch-on').toggleClass('switch-on');
+		if(!$('.switch-btn.switch-on')){
+			$('.switch-btn').trigger('of.switch');
+		}
+		$('.switch-btn2').toggleClass('switch-on2');
+		if(!$('.switch-btn2.switch-on')){
+			$('.switch-btn2').trigger('on.switch');
+		}
+		$('p').css('line-height','1');
+		$('img').css('display','block');
+				
 	}
 	$('.font-size-small').click(function(event){
 		replaceClass("html", "hfont-size-midle", "hfont-size-large","", "hfont-size-small");
 		// $('html').css("font-size",(1*100)+"%");
-		$('html').css("font-size","15px");
-		customObject.fontSize = 15;
+		$('html').css("font-size","16px");
+		customObject.fontSize = 16;
 		ajaxSend();
 		
 	});
 	$('.font-size-midle').click(function(event){
 		replaceClass("html", "hfont-size-small", "hfont-size-large","", "hfont-size-midle");
 		// $('html').css("font-size",(2*100)+"%");
-		$('html').css("font-size","25px");
-		customObject.fontSize = 25;
+		$('html').css("font-size","18px");
+		customObject.fontSize = 18;
 		ajaxSend();
 		
 	});
 	$('.font-size-large').click(function(event){
 		replaceClass("html", "hfont-size-small", "hfont-size-midle","", "hfont-size-large");
 		// $('html').css("font-size",(3*100)+"%");
-		$('html').css("font-size","35px");
-		customObject.fontSize = 35;
+		$('html').css("font-size","20px");
+		customObject.fontSize = 20;
 		ajaxSend();
 	});
 
@@ -89,10 +147,16 @@ $(document).ready(function() {
 
 	$('.font-classic').click(function(event){
 		replaceClass("html", "hfont-times-new","","","hfont-classic");
+		$('p').css("font-family",'PT Sans');
+		customObject.font = 'PT Sans';
+		ajaxSend();
 	});
 
 	$('.font-times-new').click(function(event){
 		replaceClass("html", "hfont-classic", "","", "hfont-times-new");
+		$('p').css("font-family",'Times New Roman');
+		customObject.font = 'Times New Roman';
+		ajaxSend();
 	});
 
 
@@ -108,28 +172,55 @@ $(document).ready(function() {
 	$('.switch-btn2').click(function () {
 		$(this).toggleClass('switch-on2');
 		if ($(this).hasClass('switch-on2')) {
-			$(this).trigger('on.switch2');
+			$(this).trigger('on.switch');
 		} else {
-			$(this).trigger('off.switch2');
+			$(this).trigger('off.switch');
 		}
 	});
 	$('.switch-btn').on('on.switch', function () {
 		replaceClass("html", "hcheckbox-interval-f", "","", "hcheckbox-interval-t");
+		$('html').css('letter-spacing','.1rem');
+		customObject.checkboxInterval = '.1rem';
+		ajaxSend();
 	});
 	$('.switch-btn').on('off.switch', function () {
 		replaceClass("html", "hcheckbox-interval-t", "","", "hcheckbox-interval-f");
+		$('html').css('letter-spacing','normal');
+		customObject.checkboxInterval = 'normal';
+		ajaxSend();
 	});
 
+	$('.switch-btn2').on('on.switch', function () {
+		replaceClass("html", "hcheckbox-img-f", "","", "hcheckbox-img-t");
+		$('img').css('display','block');
+		customObject.imgVisible = 'block';
+		ajaxSend();
+	});
+	$('.switch-btn2').on('off.switch', function () {
+		replaceClass("html", "hcheckbox-img-t", "","", "hcheckbox-img-f");
+		$('img').css('display','none');
+		customObject.imgVisible = 'none';
+		ajaxSend();
+	});
 
 
 	$('.between-interval-small').click(function(event){
-		replaceClass("html", "hbetween-interval-midle", "hbetween-interval-large","", "hbetween-interval-small");		
+		replaceClass("html", "hbetween-interval-midle", "hbetween-interval-large","", "hbetween-interval-small");	
+		$('p').css('line-height','1');
+		customObject.betweenInterval = 1;
+		ajaxSend();
 	});
 	$('.between-interval-midle').click(function(event){
 		replaceClass("html", "hbetween-interval-small", "hbetween-interval-large","", "hbetween-interval-midle");
+		$('p').css('line-height','1.4');
+		customObject.betweenInterval = 1.4;
+		ajaxSend();
 	});
 	$('.between-interval-large').click(function(event){
 		replaceClass("html", "hbetween-interval-small", "hbetween-interval-midle","", "hbetween-interval-large");
+		$('p').css('line-height','1.8');
+		customObject.betweenInterval = 1.8;
+		ajaxSend();
 	});
 
 
@@ -151,12 +242,7 @@ $(document).ready(function() {
 
 
 
-	$('.switch-btn2').on('on.switch2', function () {
-		replaceClass("html", "hcheckbox-img-t", "","", "hcheckbox-img-f");
-	});
-	$('.switch-btn2').on('off.switch2', function () {
-		replaceClass("html", "hcheckbox-img-f", "","", "hcheckbox-img-t");
-	});
+	
 });
 $(document).on('click','.for-visually-impaired', function(){
 	$('.conteiner-for-visually-impaired').toggleClass('active');
@@ -181,6 +267,8 @@ $(document).on('click','.classic-site-version-btn', function(){
 	$('.conteiner-for-visually-impaired.active').toggleClass('active');
 	$('.conteiner-for-visually-impaired').css("display","none");
 	$('.classic-site-version-btn').css("display","none");
+	var impairedStyle = $(".for-visually-impaired").prop("style");
+		impairedStyle.removeProperty("display");
 	var impairedStyle = $(".button-down-footer .for-visually-impaired").prop("style");
 		impairedStyle.removeProperty("display");
 	$('html').removeClass();
@@ -192,7 +280,7 @@ $(document).on('click','.classic-site-version-btn', function(){
 
 	// var impairedStyle2 = $(".for-slepix-pidorasov .for-visually-impaired").prop("style");
 	// impairedStyle2.removeProperty("display");
-
+	customObject.fontSize = 16;
 	customObject.display = "hide";
 	ajaxSend();
 	// var classicBtn = document.querySelector('.classic-site-version-btn');
